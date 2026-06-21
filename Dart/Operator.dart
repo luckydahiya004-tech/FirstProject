9f4226c078f491b356815b021fac986f6ae3da2d
luckydahiya004-tech <luckydahiya004@gmail.com>
2026-06-21 17:15:51 +0530

    Dart

---
+++ Dart/Operator.dart
@@ -0,0 +1,25 @@
+void main(){
+   
+//Operators - Help us to do Actions
+//1. Arithmetic Operators =  +,-,*,/,%
+int price = 100;
+int qty = 2;
+int TotalBill = price * qty;
+print("The total bill is $TotalBill");
+  // % - output remainder
+  print(10 % 2);//0
+  print(13 % 2);//1
+  print(1 % 3); 
+  print(2 % 8);
+
+
+
+// 2. Relational Operators / Comarison operators
+var a = 10;
+var b = 5;
+bool result = a > b;
+print(result);
+double balance = 800.00;
+bool canPay = balance >= 800;
+print(canPay);
+}
