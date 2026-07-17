5bc10e831faabca9ea0db2dcdea8fc7e64397928
luckydahiya004-tech <luckydahiya004@gmail.com>
2026-07-17 15:54:46 +0530

    Profile Card

---
+++ my_layouts/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ProfileApp());
+}
+
+//Design a Profile Card
+class ProfileApp extends StatelessWidget{
+  @override
+Widget build(BuildContext context){
+return MaterialApp(
+home: Scaffold(
+  appBar: AppBar(title: Text("Profile Card")),
+
+  body: Center(
+    child: Container(
+
+      width: 300,
+    padding: EdgeInsets.all(20),
+    margin: EdgeInsets.all(20),
+
+    decoration: BoxDecoration(
+      color: Colors.lightBlue.shade100,
+      borderRadius: BorderRadius.circular(12),
+    ),
+    child: Column(
+      //One Below each other
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text("Lucky" ,
+        style: TextStyle(
+          color: Colors.deepPurple.shade900,
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+         ),
+         ),
+
+         SizedBox(height:10 ),
+         //Add Extra Space between the Text and the Image
+        Text("App Developer"),
+        SizedBox(height: 10),
+        Text("India"),
+        TextButton(
+          onPressed: () {
+        debugPrint("Button Clicked");
+        },
+         child: Text("Follow"),
+        ),
+      ],
+
+    ),
+    ),
+
+  ),
+
+
+),
+);
+}
+}
