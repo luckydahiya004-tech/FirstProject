1af25b23578611108ccec748ee82d99b968e8600
luckydahiya004-tech <luckydahiya004@gmail.com>
2026-07-17 16:13:28 +0530

    Updated

--- my_layouts/lib/main.dart
+++ my_layouts/lib/main.dart
@@ -12,9 +12,17 @@ return MaterialApp(
 home: Scaffold(
   appBar: AppBar(title: Text("Profile Card")),
 
-  body: Center(
-    child: Container(
 
+
+
+  body: Center(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children:[
+
+
+//First Card
+Container(
       width: 300,
     padding: EdgeInsets.all(20),
     margin: EdgeInsets.all(20),
@@ -49,11 +57,54 @@ home: Scaffold(
       ],
 
     ),
-    ),
+),
+ 
 
+
+//Second Card
+Container(
+
+      width: 300,
+    padding: EdgeInsets.all(20),
+    margin: EdgeInsets.all(20),
+
+    decoration: BoxDecoration(
+      color: Colors.lightBlue.shade100,
+      borderRadius: BorderRadius.circular(12),
+    ),
+child: Column(
+      //One Below each other
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text("Skills" ,
+        style: TextStyle(
+          color: Colors.deepPurple.shade900,
+          fontSize: 22,
+          fontWeight: FontWeight.bold,
+         ),
+         ),
+
+         SizedBox(height:10 ),
+         //Add Extra Space between the Text and the Image
+        Text("App Developer"),
+        SizedBox(height: 10),
+        Text("Beginner"),
+        TextButton(
+          onPressed: () {
+        debugPrint("Button Clicked");
+        },
+         child: Text("Resume"),
+        ),
+      ],
+
+    ),
   ),
 
 
+
+      ],
+),
+),
 ),
 );
 }
